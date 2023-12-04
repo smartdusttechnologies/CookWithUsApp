a5e4b3d5b9e7fe924943b4d1460bfd2efe58373d
yashrajsmartdust <yashrajsmartdust@gmail.com>
2023-12-04 15:26:07 +0530

    Floating buttons added in Home page
    Floating buttons added in Home page

--- cookwithus-reactpoc/src/Pages/Home/Home.js
+++ cookwithus-reactpoc/src/Pages/Home/Home.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import NavigationIcon from '@mui/icons-material/Navigation';
 
 const Home = () => {
   return (
-    <div style={{
-      height:'40rem',
-      width:'100%',
-    }}>
-      
+    <div 
+      style={{
+        height:'40rem',
+        width:'100%',
+      }}
+    >
+      <Fab 
+        color="secondary"
+        style={{
+          position: 'fixed',
+          bottom: 76,
+          right: 236,
+        }}
+      >
+        <AddIcon />
+      </Fab>
+      <Fab variant="extended"
+        style={{
+          position: 'fixed',
+          bottom: 83,
+          right: 86,
+        }}
+      >
+        <NavigationIcon sx={{ mr: 1 }} />
+        Navigate
+      </Fab>
     </div>
   )
 }
