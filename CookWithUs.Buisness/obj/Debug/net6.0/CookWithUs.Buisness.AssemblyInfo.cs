079b467f0fd9c79c57b4b48db9dc07f8d00da0e6
yashrajsmartdust <yashrajsmartdust@gmail.com>
2024-01-16 12:39:33 +0530

    Project running
    Project running

--- CookWithUs.Buisness/obj/Debug/net6.0/CookWithUs.Buisness.AssemblyInfo.cs
+++ CookWithUs.Buisness/obj/Debug/net6.0/CookWithUs.Buisness.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookWithUs.Buisness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6205acbc90c0485c83d7d1237fcfb0908ca40a91")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookWithUs.Buisness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookWithUs.Buisness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
