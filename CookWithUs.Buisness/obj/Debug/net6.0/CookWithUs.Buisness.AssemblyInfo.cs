ea3cdd9458a88e347e8b295e88553a1880fb5f5e
mohittiwary05 <mohittiwary05@gmail.com>
2024-03-25 15:36:00 +0530

    first commit by mohit

--- CookWithUs.Buisness/obj/Debug/net6.0/CookWithUs.Buisness.AssemblyInfo.cs
+++ CookWithUs.Buisness/obj/Debug/net6.0/CookWithUs.Buisness.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookWithUs.Buisness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+167b8e0b9ff187f72ea69eda836194516d0867e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookWithUs.Buisness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookWithUs.Buisness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
